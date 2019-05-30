ca40556b2f73c64e2f6e3b01266590d109ec0c5b
guihenrry <guilhermehenry60@gmail.com>
2019-05-30 00:20:14 -0300

    refatorado codigo

--- script.js
+++ script.js
@@ -1,76 +1,59 @@
-const btnLigar = document.querySelector('.btn-ligar');
+const btnLigarDesliga = document.querySelector('.btn-ligar');
 const statusLanterna = document.querySelector('.lanterna .status');
 const statusBateria = document.querySelector('.status-bateria');
 const btnTrocarBateria = document.querySelector('.bateria a');
+let bateria = 100;
+let timer;
+let ligada = false; 
 
-let started = false;
+function handleLigaDesliga() {
+  if (ligada) {
+    desligarLanterna();
+  } else {
+    ligarLanterna();
+  }
+  ligada ? ligada = false : ligada = true;
+}
 
-const lanterna = {
-  carga: 100,
-  status: 'desligada',
-  ligar() {
-    if (this.status === 'desligada' && this.carga > 0) {
-      this.status = 'ligada';
-      statusLanterna.innerHTML = this.status;
-      document.body.classList.toggle('ativo');  
-    } 
-  },
-  desligar() {
-    if(this.status === 'ligada') {
-      this.status = 'desligada';
-      statusLanterna.innerHTML = this.status;
-      document.body.classList.toggle('ativo'); 
-    }
-  },
-  trocarBateria() {
-    this.carga = 100;
-    statusBateria.innerHTML = `${this.carga}%` ;
+function ligarLanterna() {
+  if (bateria > 0) {
+    document.body.classList.add('ativo');
+    statusLanterna.innerText = 'Ligada';
+    timer = setInterval(() => {
+      bateria--;
+      trocaStatusBateria();
+      if (bateria <= 0) {
+        desligarLanterna();
+      }
+    }, 1000);
+  }
+}
+
+function desligarLanterna() {
+  document.body.classList.remove('ativo');
+  statusLanterna.innerText = 'Desligada';
+  clearInterval(timer);
+}
+
+function trocarBateria() {
+  bateria = 100;
+  trocaStatusBateria();
+}
+
+function trocaStatusBateria() {
+  statusBateria.innerHTML = `${bateria}%`;
+  if(bateria === 100) {
     statusBateria.className = 'status-bateria bateria100';
+  } else if(bateria === 75) {
+    statusBateria.className = 'status-bateria bateria75';
+  } else if(bateria === 50) {
+    statusBateria.className = 'status-bateria bateria50';
+  } else if(bateria === 25) {
+    statusBateria.className = 'status-bateria bateria25';
+  } else if(bateria === 0) {
+    statusBateria.className = 'status-bateria bateria0';
   }
 }
 
-
-
-
-btnTrocarBateria.addEventListener('click', () => {
-  lanterna.trocarBateria();
-});
-
-
-btnLigar.addEventListener('click', () => {
-  if (lanterna.status === 'desligada') {
-    lanterna.ligar();
-    if(!started && lanterna.carga > 0) {
-      started = true;
-      start();
-    } else {
-      started = false;
-      start();
-    }
-  } else {
-    lanterna.desligar();
-    started = false;
-    start();
-  }
-});
-
-function start() {
-  if((lanterna.carga -1) >= 0 && started === true) {
-    lanterna.carga = lanterna.carga - 1;
-    statusBateria.innerHTML = `${lanterna.carga}%` ;
-    setTimeout('start()', 1000);
-    if(lanterna.carga === 100) {
-      statusBateria.className = 'status-bateria bateria100';
-    } else if(lanterna.carga === 75) {
-      statusBateria.className = 'status-bateria bateria75';
-    } else if(lanterna.carga === 50) {
-      statusBateria.className = 'status-bateria bateria50';
-    } else if(lanterna.carga === 25) {
-      statusBateria.className = 'status-bateria bateria25';
-    } else if(lanterna.carga === 0) {
-      statusBateria.className = 'status-bateria bateria0';
-    }
-  } else {
-    lanterna.desligar();
-  }
-}
+btnLigarDesliga.addEventListener('click', handleLigaDesliga);
+btnTrocarBateria.addEventListener('click', trocarBateria);
